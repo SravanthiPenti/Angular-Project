b5df8fbefd6f49aa7f75ebdfa84c7495e6b8b1c9
SravanthiPenti <sravanthi.penti@navaratan.com>
2018-01-09 12:01:37 +0530

    deleted all created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,39 +3,26 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {RouterModule,Routes} from '@angular/router';
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
-import { ShowuserdetailsComponent } from './showuserdetails/showuserdetails.component';
-import { UserService } from './user.service';
-import { HomeComponent } from './home/home.component';
-import {HttpModule} from '@angular/http';
-import {ShowComponent} from './home/show.component';
-export const routes:Routes=[
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  {path:'home', component:UsersComponent},
-  // {path:'details',component:HomeComponent},
-  // {path:'home/:id',component:ShowComponent},
 
-  {path:'home/:id',component:ShowuserdetailsComponent}
-]
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    ShowuserdetailsComponent,
-    HomeComponent,
-    ShowComponent
+
+   
+ 
 
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    RouterModule.forRoot(routes)
+  
  
   ],
 
   bootstrap: [AppComponent],
-  providers:[UserService],
+  providers:[],
 })
 export class AppModule { }
