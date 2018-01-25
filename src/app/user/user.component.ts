6b89238989df6add5404f77ce52866cf8acf6c6d
SravanthiPenti <sravanthi.penti@navaratan.com>
2018-01-25 16:56:34 +0530

    added pipes to the full name,date of birth in user.component.html

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,7 +8,7 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
   deleteUser: boolean = false;
