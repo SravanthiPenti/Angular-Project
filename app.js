d04c2477d523cac58f0d88d7afd346441f95055f
SravanthiPenti <sravanthi.penti@navaratan.com>
2018-02-05 20:20:13 +0530

    got all those details from Weather Api

--- app.js
+++ app.js
@@ -13,19 +13,21 @@ var allowCrossDomain = function (req, res, next) {
 	}
 }
 
+global.axios = require('axios');
+
 global.express = require('express')
 global.app = express()
 bodyParser = require('body-parser');
-var fs=require('fs');
-global.path=require("path");
+var fs = require('fs');
+global.path = require("path");
 global.objectID = require('mongodb').ObjectID;
 app.use(bodyParser.json());
 app.use(allowCrossDomain);
-var multipart=require("connect-multiparty");
+var multipart = require("connect-multiparty");
 
-var lowercase=require('lower-case');
+var lowercase = require('lower-case');
 app.use(multipart({
-	uploadDir:path.join(__dirname,'./src/assets/uploads')
+	uploadDir: path.join(__dirname, './src/assets/uploads')
 }));
 
 global.MongoClient = require('mongodb').MongoClient
@@ -41,21 +43,51 @@ MongoClient.connect(url, function (err, db) {
 	global.db = db.db("mongoNode");
 });
 
+app.get('/check', function (req, res) {
+	url = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDo74DJNZN4YKthmD5h-AJo6jESFJoeA7o"
+	axios.get(url, { json: true }).then(function (response) {
+		// res.send(response);
+		var path = response.data.results[0].geometry.location;
+		// lng ,lat in location
+		console.log(path);
+		console.log(path.lng);
+		console.log(path.lat);
+		var latitude1 = path.lat;
+		var longitude1 = path.lng;
+		console.log(latitude1);
 
-app.post('/upload', function(req, res){
+		url = "https://api.darksky.net/forecast/47d2c14d5f765e08c3cef24b0cea30a9/" + latitude1 + "," + longitude1;
+		axios.get(url, { json: true }).then(function (response) {
+			console.log(response);
+			var temp = response.data.currently.temperature;
+			var time = response.data.currently.time;
+			var date = new Date(time);
+			console.log(date);
+			var celsius = (temp - 32) * (5 / 9);
+			console.log(celsius);
+			res.status(200).send({ lat: latitude1, lon: longitude1, temp: temp, celc: celsius });
+		}, function () {
+			console.log(error);
+		});
+	}, function (error) {
+		console.log(error);
+	});
+});
+
+app.post('/upload', function (req, res) {
 	console.log(req.files);
-	console.log("path:"+req.files.imageFile.path);
-    var location=req.files.imageFile.path;
-	console.log("path variable"+location);
-	var temp=new Array();
-	temp=location.split("/");
+	console.log("path:" + req.files.imageFile.path);
+	var location = req.files.imageFile.path;
+	console.log("path variable" + location);
+	var temp = new Array();
+	temp = location.split("/");
 	console.log(temp);
-	var image=new Array();
-	image=temp.reverse();
+	var image = new Array();
+	image = temp.reverse();
 	console.log(image);
-	console.log("ImagePath:"+image[0]);
-	global.imagePath=image[0];
-	res.send({status:true});
+	console.log("ImagePath:" + image[0]);
+	global.imagePath = image[0];
+	res.send({ status: true });
 })
 
 app.post('/adduser', function (req, res) {
@@ -64,10 +96,10 @@ app.post('/adduser', function (req, res) {
 		fullname: lowercase(req.body.fullname),
 		email: lowercase(req.body.email),
 		hobbies: lowercase(req.body.hobbies),
-		dateofBirth:req.body.dateofBirth,
-		file:imagePath,
-		status:true
-		
+		dateofBirth: req.body.dateofBirth,
+		file: imagePath,
+		status: true
+
 	}
 
 	db.collection('user').insertOne(insertdata, function (error, data) {
@@ -110,7 +142,7 @@ app.put('/home/:id', function (req, res) {
 		fullname: lowercase(req.body.fullname),
 		email: lowercase(req.body.email),
 		hobbies: lowercase(req.body.hobbies),
-		dateofBirth:req.body.dateofBirth
+		dateofBirth: req.body.dateofBirth
 	}
 	var id = req.params.id;
 	// console.log(id);
