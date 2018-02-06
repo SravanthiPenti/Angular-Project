4fc1b145bcd2aaee9de4d48b4cad6c0fc1558907
SravanthiPenti <sravanthi.penti@navaratan.com>
2018-02-06 18:39:24 +0530

    Took api.js to Routing for all User Operations(delete,update,add,get)

--- server/api/api.js
+++ server/api/api.js
@@ -1,22 +1,11 @@
-// var MongoClient=require("mongodb").MongoClient;
-// var bodyparser=require("body-parser");
-// var lw=require("lower-case");
-// app.use(bodyparser,{encodeURI:true});
-// var url="mongodb://localhost:27017/";
-
-// MongoClient.connect(url,function(req,res){
-// var dbo=db.db("mongoNode");
-// })
 const api = express.Router();
-var callback = function(req, res, next){
-    if(req.count) {
-        req.count+=1
-    } else {
-        req.count = 1
-    }
-    next();
-};
-api.get('/',callback,callback,callback,callback,callback,callback,callback, function(req, res){
-    console.log("final count: ", req.count);
-})
+const gitController = require('../controllers/git');
+const userController=require('../controllers/users');
+const weatherController=require('../controllers/weather')
+
+
+
+api.use('/user',userController);
+api.use('/git',gitController);
+api.use('/weather',weatherController);
 module.exports = api;
